b30936c0ccac21808edd2349499bc16c78fb591c
Akilesh <akileshakileshs1234@gmail.com>
2026-05-29 15:06:38 +0530

    update common library patterns and constants

--- nestjs-backend/libs/common/src/patterns/index.ts
+++ nestjs-backend/libs/common/src/patterns/index.ts
@@ -2,3 +2,6 @@ export * from './auth.patterns';
 export * from './user.patterns';
 export * from './staff.patterns';
 export * from './item.patterns';
+export * from './location.patterns';
+export * from './market.patterns';
+export * from './dealer.patterns';
---
+++ nestjs-backend/libs/common/src/patterns/market.patterns.ts
@@ -0,0 +1,6 @@
+export const MARKET_CREATE = 'market.create';
+export const MARKET_FIND_ALL = 'market.findAll';
+export const MARKET_FIND_ONE = 'market.findOne';
+export const MARKET_UPDATE = 'market.update';
+export const MARKET_DELETE = 'market.delete';
+export const MARKET_FIND_ACTIVE = 'market.findActive';
